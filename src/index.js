0837feb2fc10aef2f810e310f5f9272b80db8982
chenzhiying <1262922459@qq.com>
2023-08-05 17:30:49 +0800

    初始化项目&完成createElement方法

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import ReactDOM from 'react-dom';
+// import React from 'react';
+import React from './react';
+
+// ReactDOM.render(
+//   <div> Hello Simple React </div>,
+//   document.getElementById('root')
+// );
+
+console.log(
+  <div>
+    Hello Simple React <span>111</span>
+    <span>22222</span>
+  </div>
+);
